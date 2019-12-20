9fbacad53460f34da00445df17b88c2bde9b31a9
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-12-19 20:43:31 -0500

    fix: updated pytest configuration

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ def create_app(env_name):
 
     # app initiliazation
     app = Flask(__name__)
+    app.app_context().push()
 
     app.config.from_object(app_config[env_name])
     # initializing bcrypt
